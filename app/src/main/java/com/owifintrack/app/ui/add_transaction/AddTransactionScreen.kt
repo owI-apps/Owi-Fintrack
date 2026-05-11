39b284bb01473e62a76ec0b2955a62626653d1c4
owI-apps <fciowi.aplikasi@gmail.com>
2026-05-11 17:44:32 +0700

    Create AddTransactionScreen.kt

---
+++ app/src/main/java/com/owifintrack/app/ui/add_transaction/AddTransactionScreen.kt
@@ -0,0 +1,10 @@
+package com.owifintrack.app.ui.add_transaction
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AddTransactionScreen(onBackClick: () -> Unit) {
+    // Ini cangkang sementara, nanti kita buat Form-nya
+    Text(text = "Halaman Tambah Transaksi - Cangkang")
+}
