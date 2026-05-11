4a464ddecea9aa48f8d7ef94b0ee50c1106ada1b
owI-apps <fciowi.aplikasi@gmail.com>
2026-05-11 17:45:19 +0700

    Create HistoryScreen.kt

---
+++ app/src/main/java/com/owifintrack/app/ui/history/HistoryScreen.kt
@@ -0,0 +1,10 @@
+package com.owifintrack.app.ui.history
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HistoryScreen(onBackClick: () -> Unit) {
+    // Ini cangkang sementara, nanti kita buat List Transaksinya
+    Text(text = "Halaman Riwayat - Cangkang")
+}
