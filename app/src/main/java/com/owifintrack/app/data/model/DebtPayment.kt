10c61d1a7b81ca4e1e02893e6cf50cf689033fd6
owI-apps <fciowi.aplikasi@gmail.com>
2026-05-12 08:34:31 +0700

    Delete DebtPayment.kt

--- app/src/main/java/com/owifintrack/app/data/model/DebtPayment.kt
+++
@@ -1,9 +0,0 @@
-package com.owifintrack.app.data.model
-
-data class DebtPayment(
-    val id: Int = 0,
-    val debtId: Int, // Terhubung ke Debt mana
-    val amount: Double, // Nominal pembayaran kali ini
-    val date: Long,
-    val note: String = ""
-)
